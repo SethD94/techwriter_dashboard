a2d51f5f83eb52988f0b3c39a080c02d3a56aa2f
Antonio Castro <antonio@masterycoding.com>
2021-04-03 22:46:38 -0500

    Example

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import router from 'next/router'
-import { Container, Row, Col, Card, Button, CardText, CardTitle, CardHeader } from 'shards-react'
+import { Container, Row, Col, Card, Button, CardText, CardTitle, CardHeader, CardBody, InputGroup, FormInput, FormGroup, Collapse } from 'shards-react'
 
 export default function Home() {
 
   const handleRedirectToUser = () => {
     router.push('/users');
   }
+
+  const [information, setInformation] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleInfoChange = (e) => {
+    const { target } = e;
+
+    setInformation({ ...information, [e.target.name]: e.target.value })
+  }
+
+
   return (
     <Container>
       <Head>
@@ -18,7 +35,29 @@ export default function Home() {
         <Col>
           <Card>
             <CardHeader>Home Page</CardHeader>
-            <Button onClick={handleRedirectToUser}>Users Page</Button>
+            <CardBody>
+            <FormGroup>
+                <label>First Name</label>
+                <FormInput name={'firstName'} onChange={handleInfoChange} value={information.firstName} />
+              </FormGroup>
+
+              <FormGroup>
+                <label>Last Name</label>
+                <FormInput name={'lastName'} onChange={handleInfoChange} value={information.lastName} />
+              </FormGroup>
+
+              <FormGroup>
+                <label>Email</label>
+                <FormInput name={'email'} onChange={handleInfoChange} value={information.emails} />
+              </FormGroup>
+
+              <Button onClick={() => setIsToggled(!isToggled)}>Toggle</Button>
+              <Collapse open={isToggled}>
+                <p>{information.firstName}</p>
+                <p>{information.lastName}</p>
+                <p>{information.email}</p>
+              </Collapse>
+            </CardBody>
           </Card>
         </Col>
       </Row>
