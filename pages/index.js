bfd084aea3a623866b433ef1af8c3aefbae922a0
Seth <sethdelpachitra@gmail.com>
2021-04-16 10:50:44 +1200

    Attempting to compartmentalize app components

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
+import React from 'react';
 import { useState } from 'react';
-import Head from 'next/head'
-import router from 'next/router'
-import { Container, Row, Col, Card, Button, CardText, CardTitle, CardHeader, CardBody, InputGroup, FormInput, FormGroup, Collapse } from 'shards-react'
-import {
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  InputGroupAddon,
-  InputGroupText,
-} from "shards-react";
+import Head from 'next/head';
+import router from 'next/router';
+import NavigationBar from './components';
 
 export default function Home() {
 
@@ -23,37 +10,13 @@ export default function Home() {
     router.push('/users');
   };
 
-  const [open, setOpen] = useState(false);
-  const toggle = () => {
-    setOpen((c) => !c);
-  };
-   const[dropdown, setdropdown] = useState("Writer");
-   const showUser = (e) =>{
-    setdropdown(e.target.value);
-  }
-  return (
-    <div>
-      <Head>
-        <title>Working Tile</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar type="dark" theme="dark" expand="md">
-        <NavbarBrand href="#">Shards React</NavbarBrand>
-        <NavbarToggler/>
-          <Nav navbar className="ml-auto">
-            <Dropdown open={open} toggle={toggle}>
-              <DropdownToggle nav caret value={dropdown}>{dropdown}</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem value="All" onClick={showUser}>All</DropdownItem>
-                  <DropdownItem value="Dennis" onClick={showUser}>Dennis Thorpe</DropdownItem>
-                  <DropdownItem value="Guy" onClick={showUser}>Guy Halpe</DropdownItem>
-                  <DropdownItem value="Janet" onClick={showUser}>Janet Stevenson</DropdownItem>
-                  <DropdownItem value="Paul" onClick={showUser}>Paul Erith</DropdownItem>
-                  <DropdownItem value="Seth" onClick={showUser}>Seth Delpachitra</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
-          </Nav>
-      </Navbar>
-    </div>
-  )
+   return (
+      <div>
+        <Head>
+          <title>Working Tile</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        return <NavigationBar/>;
+      </div>
+    )
 }
